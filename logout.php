3491abf379222cd5fd5bc647ef1bb7914a24b9b6
ic-chen <mimi2344@hotmail.com>
2019-11-01 09:35:24 +0800

    取消cookie

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
-session_start();
-unset($_SESSION['login']);
-unset($_SESSION['id']);
+// session_start();
+// unset($_SESSION['login']);
+// unset($_SESSION['id']);
+// header("location:index.php");
+
+setcookie("login","",time()-1);
+setcookie("id","",time()-1);
 header("location:index.php");
 ?>
